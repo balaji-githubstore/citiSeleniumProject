a0b893a3fd1c6a8ece31c7ceba3ba9b1e1eeae10
balaji-githubstore <balaji0017@gmail.com>
2021-03-10 13:02:14 +0530

    s-commit

--- src/com/citi/magento/LoginTest.java
+++ src/com/citi/magento/LoginTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-
 public class LoginTest {
 
 	public static void main(String[] args) {
